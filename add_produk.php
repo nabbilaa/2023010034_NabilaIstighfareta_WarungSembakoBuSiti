52a2d5a09d199bb7fc1f84c9f5d81c3ab6499a5c
nabbilaa <nistighfareta@gmail.com>
2026-05-01 16:12:53 +0700

    update

--- add_produk.php
+++ add_produk.php
@@ -3,7 +3,6 @@ include 'config/koneksi.php';
 
 $message = "";
 
-// Mengambil data kategori
 $result_kategori = $conn->query("SELECT * FROM kategori");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
--- index.php
+++ index.php
@@ -6,33 +6,27 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-  <!-- Bootstrap Icon -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 
   <title>Warung Sembako Bu Siti</title>
 
-  <!-- Favicons -->
   <link href="assets/img/logo_warung.png" rel="icon">
 
-  <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
     rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
   <link href="assets/css/main.css" rel="stylesheet">
 </head>
 
@@ -66,7 +60,6 @@
 
     <main class="main">
 
-      <!-- Home -->
       <section id="hero" class="hero section">
         <div class="container">
           <div class="row gy-4">
@@ -87,7 +80,6 @@
           </div>
         </div>
       </section>
-      <!-- End Home -->
 
       <?php
       include "config/koneksi.php";
@@ -179,7 +171,7 @@
             <div class="col-lg-3 d-flex flex-column align-items-center">
               <div class="step">
                 <i class="bi bi-cart"></i>
-                <h4>Pilih & Pesan</h4>
+                <h4>Pilih Produk</h4>
                 <p>Pilih produk yang Anda inginkan dan lakukan pemesanan</p>
               </div>
             </div>
@@ -431,11 +423,9 @@
     </footer>
     <!-- End Footer -->
 
-    <!-- Scroll Top -->
     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Preloader -->
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
@@ -448,7 +438,6 @@
     <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
     <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
 
-    <!-- Main JS File -->
     <script src="assets/js/main.js"></script>
 
   </body>
